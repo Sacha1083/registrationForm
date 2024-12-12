d44d30553efcd28d2fb31083d6c6a8b838483147
Sacha1083 <87449379+Sacha1083@users.noreply.github.com>
2024-12-12 15:40:58 +0100

    Update TextFont.java

--- src/main/java/util/TextFont.java
+++ src/main/java/util/TextFont.java
@@ -2,6 +2,14 @@ package util;
 
 import java.awt.*;
 
+/**
+ * TextFont class to set the font of the text
+ * @return Font
+ * @see Font
+ * @autor Sacha1083
+ * @version 2.0
+ * @since JDK21.0.5
+ */
 public class TextFont {
     public static Font titleFont() {
         return new Font("Segoe UI", Font.BOLD, 24);
