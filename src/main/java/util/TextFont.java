5e260b72b3446b90e7c0d221fa55a47890275c22
Sacha1083 <87449379+Sacha1083@users.noreply.github.com>
2024-12-12 16:03:23 +0100

    TextFont javadoc

--- src/main/java/util/TextFont.java
+++ src/main/java/util/TextFont.java
@@ -11,14 +11,30 @@ import java.awt.*;
  * @since JDK21.0.5
  */
 public class TextFont {
+
+    /**
+     * titleFont method to set the font of the title
+     * @see Font
+     * @return Font
+     */
     public static Font titleFont() {
         return new Font("Segoe UI", Font.BOLD, 24);
     }
 
+    /**
+     * textFont method to set the font of the text
+     * @see Font
+     * @return Font
+     */
     public static Font textFont() {
         return new Font("Segoe UI", Font.BOLD, 12);
     }
 
+    /**
+     * textFormFont method to set the font of the text form
+     * @see Font
+     * @return Font
+     */
     public static Font textFormFont() {
         return new Font("Segoe UI", Font.BOLD, 16);
     }
